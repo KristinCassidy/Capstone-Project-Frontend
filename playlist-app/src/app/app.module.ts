87093f2ffb5720a6a6d8062f14ff6eb430c6df36
Kristin Cassidy <kristin.leigh.cassidy@gmail.com>
2021-03-25 12:29:20 -0500

    angular forms

--- playlist-app/src/app/app.module.ts
+++ playlist-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { SplashComponent } from './splash/splash.component';
 import { HeadbarComponent } from './headbar/headbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CreateCollectionComponent } from './create-collection/create-collection.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { CreateCollectionComponent } from './create-collection/create-collection
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
