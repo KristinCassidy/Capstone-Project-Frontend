b0365f30ef796272384a128efac40944f3e16095
Kristin Cassidy <kristin.leigh.cassidy@gmail.com>
2021-03-25 12:29:20 -0500

    angular forms

--- playlist-app/src/app/create-collection/create-collection.component.ts
+++ playlist-app/src/app/create-collection/create-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-collection',
@@ -12,4 +13,7 @@ export class CreateCollectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log('Submitted!')
+  }
 }
